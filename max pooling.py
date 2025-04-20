1237fdac0b6276dd00804502214fc64d171a4832
En1zai <157814882+En1zai@users.noreply.github.com>
2025-04-20 12:00:37 +0700

    Максимальное объединение

---
+++ max pooling.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+def max_pooling(array, window_size=2, stride=2):
+    """
+    Параметры:
+    input_data - передаваемый 2D-массив
+    window_size - размер окна (х2)
+    stride - размер шага окна (2)
+    """
+
+    # Вычисляем размеры выходного изображения и создаем пустой массив для результата
+    h = (array.shape[0] - window_size) // stride + 1
+    w = (array.shape[1] - window_size) // stride + 1
+    output = np.zeros((h, w), dtype=int)
+
+    # Используем Max_Pooling
+    for i in range(h):
+        for j in range(w):
+            # Выделяем текущее окно
+            window = array[
+                     i * stride: i * stride + window_size,
+                     j * stride: j * stride + window_size
+                     ]
+            # Получаем максимум в текущем окне и записываем в массив
+            output[i, j] = np.max(window)
+
+    return output
+
+
+# Черно-белое изображение, каждое число массива - интенсивность пикселя
+image_pixel = np.array([
+    [1, 2, 3, 5],
+    [5, 6, 7, 5],
+    [9, 10, 11, 4],
+    [13, 14, 15, 2]
+])
+
+#Вывод результата алгоритма
+output_image = max_pooling(image_pixel)
+print("До Max Pooling:\n", image_pixel)
+print("После Max Pooling:\n", output_image)
